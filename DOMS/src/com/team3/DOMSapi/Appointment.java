3ee8b77a1aabbf0efa17b40616315e75c40ff5ec
tristanwalk <tristanwalk1@gmail.com>
2019-09-30 15:18:30 -0400

    Created Appointment class

--- DOMS/src/com/team3/DOMSapi/Appointment.java
+++ DOMS/src/com/team3/DOMSapi/Appointment.java
@@ -1,5 +1,30 @@
 package com.team3.DOMSapi;
 
-public class Appointment {
+import java.sql.Date;
+import java.sql.Time;
 
+public class Appointment {
+	Date date;
+	Time time;
+
+	public Appointment(Date apptDate, Time apptTime) {
+		date = apptDate;
+		time = apptTime;
+	}
+	
+	public Time getTime() {
+		return time;
+	}
+	
+	public void setTime(Time apptTime) {
+		this.time = apptTime;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date apptDate) {
+		this.date = apptDate;
+	}
 }
