21364b61018a74c4fb43c07a88b49cdef6d79664
tristanwalk <tristanwalk1@gmail.com>
2019-10-02 16:10:32 -0400

    Added DOMSdb sql query file. Updated attribute names for Appointment.

---
+++ DOMS/db/DOMSdb.sql
@@ -0,0 +1,24 @@
+#create schema DOMSdb;
+/*
+create table Patient(
+	patientName varchar(50),
+    birthDate date,
+    ssn varchar(11),
+    allergies varchar(500),
+    preferredDoctor varchar(50),
+    bloodtype varchar(3),
+    primary key(ssn)
+);
+    
+ create table Appointment(
+	apptDate date,
+    apptTime datetime,
+    notes varchar(500),
+    status varchar(9)
+ );
+ 
+ create table AppointmentManager(
+	name varchar(50),
+    birthDate date
+ );
+ */
