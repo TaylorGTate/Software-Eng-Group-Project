f6917416073e7c59f0f8e5c38ec1ef3c6efd183f
tristanwalk <tristanwalk1@gmail.com>
2019-10-03 16:09:29 -0400

    Updated sql query file.

--- DOMS/db/DOMSdb.sql
+++ DOMS/db/DOMSdb.sql
@@ -11,14 +11,26 @@ create table Patient(
 );
     
  create table Appointment(
+	appt_id int(11) AUTO_INCREMENT,
+    Pssn varchar(11),
 	apptDate date,
     apptTime datetime,
     notes varchar(500),
-    status varchar(9)
+    status varchar(9),
+    primary key(appt_id),
+    foreign key(Pssn) references Patient(ssn)
  );
  
  create table AppointmentManager(
+	manager_id int(11) AUTO_INCREMENT,
 	name varchar(50),
-    birthDate date
+    birthDate date,
+    primary key(manager_id)
  );
  */
+  #select * from Patient;
+  /*
+  drop table Patient;
+  drop table Appointment;
+  drop table AppointmentManager;
+  */
