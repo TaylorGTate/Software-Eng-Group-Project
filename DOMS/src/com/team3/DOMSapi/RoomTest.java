46c471c613e96ba79808201ea1a69d4b847db60b
taylortate4 <tgtate@valdosta.edu>
2019-10-11 17:48:07 -0400

    Added junit test class for room and wrote tests for room class methods

---
+++ DOMS/src/com/team3/DOMSapi/RoomTest.java
@@ -0,0 +1,44 @@
+package com.team3.DOMSapi;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class RoomTest {
+
+	@Test
+	void testRoom() {
+		Room room = new Room(1, "Clean and Ready");
+		int expectedRoomNumber = 1;
+		int actualRoomNumber = room.getRoomNumber();
+		assertEquals(expectedRoomNumber, actualRoomNumber);
+	}
+
+	@Test
+	void testGetRoomNumber() {
+		Room room = new Room(1, "Clean and Ready");
+		int expectedRoomNumber = 1;
+		int actualRoomNumber = room.getRoomNumber();
+		assertEquals(expectedRoomNumber, actualRoomNumber);
+	}
+
+	@Test
+	void testSetRoomNumber() {
+		Room room = new Room(1, "Clean and Ready");
+		room.setRoomNumber(2);
+		int expectedRoomNumber = 2;
+		int actualRoomNumber = room.getRoomNumber();
+		assertEquals(expectedRoomNumber, actualRoomNumber);
+	}
+
+	@Test
+	void testIsAvaliable() {
+		Room room = new Room(1, "Clean and Ready");
+		String expectedAvaliblity = "Clean and Ready";
+		String actualAvaliblity = room.isAvaliable();
+		assertEquals(expectedAvaliblity, actualAvaliblity);
+	}
+
+}
