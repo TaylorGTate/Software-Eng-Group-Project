23b96c3c3c630337fb80c3eafc52e7bf1b82e998
mbwood14 <48263206+mbwood14@users.noreply.github.com>
2019-10-15 02:02:12 -0400

    Changed Room manager
    added better syntax and indication of which tests don't work

--- DOMS/src/com/team3/DOMSapi/RoomManagerTest.java
+++ DOMS/src/com/team3/DOMSapi/RoomManagerTest.java
@@ -41,6 +41,19 @@ class RoomManagerTest {
 		String actualName = roomManager.getName();
 		assertEquals(expectedName, actualName);
 	}
+	
+	@Test
+	void testSetBirthday() {
+		RoomManager roomManager = new RoomManager (1, "Mitchell", "1996-4-14");
+		String expectedBirthday = "1996-4-14";
+		String actualBirthday = roomManager.getBirthday();
+		assertEquals(expectedBirthday, actualBirthday);
+	}
+	
+	//****************************************************
+	//****************************************************
+	//These are the ones that I copy pasted the code for syntax
+	//all of the methods below here give false positive tests
 
 	@Test
 	void testSetName() {
@@ -49,53 +62,47 @@ class RoomManagerTest {
 		RoomManager.setName("Mitchell");
 		assertEquals(expectedName, RoomManager.getName());
 	}
-
-	@Test
-	void testSetBirthday() {
-		RoomManager roomManager = new RoomManager (1, "Mitchell", "1996-4-14");
-		String expectedName = "Mitchell";
-		String actualName = roomManager.getName();
-		assertEquals(expectedName, actualName);
-	}
+	
 
 	@Test
 	void testGetBirthday() {
 		RoomManager roomManager = new RoomManager (1, "Mitchell", "1996-4-14");
-		String expectedName = "Mitchell";
-		String actualName = roomManager.getName();
-		assertEquals(expectedName, actualName);
+		String expectedBirthDay = "1996-4-14";
+		String actualBirthday = roomManager.setBirthday["1996-4-14"]();
+		assertEquals(expectedBirthday, actualBirthday);
 	}
 
 	@Test
 	void testGetRoomStatus() {
+		room.avaliable = status[1];
 		RoomManager roomManager = new RoomManager (1, "Mitchell", "1996-4-14");
-		String expectedName = "Mitchell";
-		String actualName = roomManager.getName();
-		assertEquals(expectedName, actualName);
+		String expectedRoomStatus = roomManager.getRoomStatus(1);
+		String actualRoomStatus = roomManager.getRoomStatus();
+		assertEquals(expectedRoomStatus, actualRoomStatus);
 	}
 
 	@Test
 	void testSetRoomStatusToClean() {
 		RoomManager roomManager = new RoomManager (1, "Mitchell", "1996-4-14");
-		String expectedName = "Mitchell";
-		String actualName = roomManager.getName();
-		assertEquals(expectedName, actualName);
+		String expectedRoomStatus = "Clean";
+		String actualRoomStatus = roomManager.getRoomStatus(Room room();
+		assertEquals(expectedRoomStatus, actualRoomStatus);
 	}
 
 	@Test
 	void testSetRoomStatusToOccupied() {
 		RoomManager roomManager = new RoomManager (1, "Mitchell", "1996-4-14");
-		String expectedName = "Mitchell";
-		String actualName = roomManager.getName();
-		assertEquals(expectedName, actualName);
+		String expectedRoomStatus = "Occupied";
+		String actualRoomStatus = roomManager.setRoomStatus[2]();
+		assertEquals(expectedRoomStatus, actualRoomStatus);
 	}
 
 	@Test
 	void testSetRoomStatusToDirty() {
 		RoomManager roomManager = new RoomManager (1, "Mitchell", "1996-4-14");
-		String expectedName = "Mitchell";
-		String actualName = roomManager.getName();
-		assertEquals(expectedName, actualName);
+		String expectedRoomStatus = "Dirty";
+		String actualRoomStatus = roomManager.setRoomStatus[3]();
+		assertEquals(expectedRoomStatus, actualRoomStatus);
 	}
 
 }
